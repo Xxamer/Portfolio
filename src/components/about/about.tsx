cbb1d6b6c3d1e021a634a386db2d4dfc8b4119fd
Christian Granado Rodriguez <chrisgrarod96@gmail.com>
2022-09-05 14:05:25 +0100

    First commit, structure added and some work

---
+++ src/components/about/about.tsx
@@ -0,0 +1,11 @@
+import './about.css';
+
+function AboutSection() {
+  return (
+    <div>
+        <h1>Ponte a bailarlo si quieres</h1>
+    </div>
+  );
+}
+
+export default AboutSection;
---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,12 @@
+import './hero.css';
+function HeroSection() {
+  return (
+    <section className='HeroBackground'>
+      <div className='HeroText'>
+        <h1>Christian Granado</h1>
+      </div>
+    </section>
+  );
+}
+
+export default HeroSection;
